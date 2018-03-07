8cfcb5eea2d84da3c68ee725bc6b3e389659670c
Tuan Vu <tuan.vu@dirox.net>
2018-03-07 23:05:52 +0700

    Configuring api routing

--- src/AppBundle/Controller/V1/AuthController.php
+++ src/AppBundle/Controller/V1/AuthController.php
@@ -2,10 +2,11 @@
 
 namespace AppBundle\Controller\V1;
 
-use FOS\RestBundle\Controller\Annotations\Route;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
 use Swagger\Annotations as SWG;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * AuthController.
@@ -15,18 +16,20 @@ class AuthController extends FOSRestController
     /**
      * Login for authentication.
      *
-     * @Route("/auth/v1/login", methods={"POST"})
      * @SWG\Response(
      *     response=200,
      *     description="Login for authentication",
      * )
-     * @SWG\Parameter(name="username", type="string", in="formData", description="Username")
-     * @SWG\Parameter(name="password", type="string", in="formData", description="Password")
      * @SWG\Tag(name="/auth/v1")
      *
-     * @param Request $request
+     * @RequestParam(name="username", description="Username")
+     * @RequestParam(name="password", description="Password")
+     *
+     * @Post("/login", name="app.user.cget")
+     *
+     * @param ParamFetcher $fetcher
      */
-    public function loginAction(Request $request)
+    public function loginAction(ParamFetcher $fetcher)
     {
     }
 }
--- src/AppBundle/Controller/V1/UserController.php
+++ src/AppBundle/Controller/V1/UserController.php
@@ -3,19 +3,46 @@
 namespace AppBundle\Controller\V1;
 
 use AppBundle\Entity\User;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Put;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Routing\ClassResourceInterface;
 
-class UserController extends FOSRestController
+/**
+ * UserController.
+ */
+class UserController extends FOSRestController implements ClassResourceInterface
 {
+    /**
+     * Get user list.
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Get user list",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=User::class, groups={"full"})
+     *     )
+     * )
+     * @SWG\Tag(name="/api/v1/user")
+     *
+     * @Get("/user")
+     */
+    public function cgetAction()
+    {
+        return 'Hello world!';
+    }
+
     /**
      * Get the specified user information.
      *
      * This call takes into account all confirmed awards, but not pending or refused awards.
      *
-     * @Route("/api/v1/{user}", methods={"GET"})
      * @SWG\Response(
      *     response=200,
      *     description="Returns the information of an user",
@@ -24,17 +51,81 @@ class UserController extends FOSRestController
      *         @Model(type=User::class, groups={"full"})
      *     )
      * )
-     * @SWG\Parameter(
-     *     name="order",
-     *     in="query",
-     *     type="string",
-     *     description="The field used to order rewards"
-     * )
      * @SWG\Tag(name="/api/v1/user")
+     *
+     * @Get("/user/{user}")
      *
      * @param User $user
      */
     public function getAction(User $user)
+    {
+        // ...
+    }
+
+    /**
+     * Create a new user.
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Create a new user",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=User::class, groups={"full"})
+     *     )
+     * )
+     * @SWG\Tag(name="/api/v1/user")
+     *
+     * @Post("/user")
+     *
+     * @param Request $request
+     */
+    public function postAction(Request $request)
+    {
+        // ...
+    }
+
+    /**
+     * Update an exist user.
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Update an exist user",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=User::class, groups={"full"})
+     *     )
+     * )
+     * @SWG\Tag(name="/api/v1/user")
+     *
+     * @Put("/user/{user}")
+     *
+     * @param User    $user
+     * @param Request $request
+     */
+    public function putAction(User $user, Request $request)
+    {
+        // ...
+    }
+
+    /**
+     * Delete an exist user.
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Delete an exist user",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=User::class, groups={"full"})
+     *     )
+     * )
+     * @SWG\Tag(name="/api/v1/user")
+     *
+     * @Delete("/user/{user}")
+     *
+     * @param User    $user
+     * @param Request $request
+     */
+    public function deleteAction(User $user, Request $request)
     {
         // ...
     }
