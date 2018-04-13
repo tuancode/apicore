11450aeebb19d8efd787624f93f567537622e01b
Tuan Vu <tuan.vu@dirox.net>
2018-04-13 08:14:31 +0700

    Add symfony module to unit test and add codeception specify package

--- src/AppBundle/Tests/unit/Entity/UserTest.php
+++ src/AppBundle/Tests/unit/Entity/UserTest.php
@@ -2,57 +2,95 @@
 
 namespace AppBundle\Entity;
 
+use Codeception\Module\Symfony;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 /**
  * UserTest.
  */
 class UserTest extends \Codeception\Test\Unit
 {
+    use \Codeception\Specify;
+
     /**
      * @var \AppBundle\UnitTester
      */
     protected $tester;
 
-    /**
-     * Test setter and getter methods.
-     */
-    public function testSetterAndGetter(): void
+    public function testGetId(): void
     {
         $user = new User();
-
-        // Id
         $this->assertNull($user->getId());
+
         $user->setId(1);
         $this->assertEquals(1, $user->getId());
+    }
 
-        // Email
+    public function testGetUsernameAndEmail(): void
+    {
+        $user = new User();
+        $this->assertNull($user->getUsername());
         $this->assertNull($user->getEmail());
+
         $user->setEmail('test@test.net');
         $this->assertEquals('test@test.net', $user->getEmail());
+        $this->assertEquals('test@test.net', $user->getUsername());
+    }
 
-        // Password
+    public function testGetPassword(): void
+    {
+        $user = new User();
         $this->assertNull($user->getPassword());
+
         $user->setPassword('123456');
         $this->assertEquals('123456', $user->getPassword());
+    }
 
-        // Status
+    public function testGetStatus(): void
+    {
+        $user = new User();
         $this->assertEquals($user->getStatus(), User::STATUS_ACTIVE);
+
         $user->setStatus(User::STATUS_SUSPENDED);
         $this->assertEquals(User::STATUS_SUSPENDED, $user->getStatus());
+    }
 
-        // Enabled
+    public function testIsEnabled(): void
+    {
+        $user = new User();
         $this->assertEquals($user->isEnabled(), User::DISABLED);
+
         $user->setEnabled(User::ENABLED);
         $this->assertEquals(User::ENABLED, $user->isEnabled());
+    }
 
-        // Phone
+    public function testGetRoles(): void
+    {
+        $user = new User();
+        $this->assertEquals([User::ROLE_DEFAULT], $user->getRoles());
+
+        $user->setRoles([User::ROLE_SUPER_ADMIN]);
+        $this->assertEquals([User::ROLE_SUPER_ADMIN, User::ROLE_DEFAULT], $user->getRoles());
+    }
+
+    public function testGetPhone(): void
+    {
+        $user = new User();
         $this->assertNull($user->getPhone());
+
         $user->setPhone('+841208772134');
         $this->assertEquals('+841208772134', $user->getPhone());
+    }
 
-        // Created Date
+    public function testGetCreatedDate(): void
+    {
+        $user = new User();
         $this->assertEquals(date('Y-m-d'), $user->getCreatedDate()->format('Y-m-d'));
+    }
 
-        // Updated Date
+    public function testGetUpdatedDate(): void
+    {
+        $user = new User();
         $this->assertEquals(date('Y-m-d'), $user->getUpdatedDate()->format('Y-m-d'));
     }
 
@@ -61,5 +99,19 @@ class UserTest extends \Codeception\Test\Unit
      */
     public function testValidation(): void
     {
+        $this->specify('Email is required', function () {
+            $user = new User();
+            $user->setEmail(null);
+
+            $this->getValidator()->validate($user);
+        });
+    }
+
+    /**
+     * @return ValidatorInterface
+     */
+    private function getValidator(): ValidatorInterface
+    {
+        return $this->tester->grabService('validator');
     }
 }
